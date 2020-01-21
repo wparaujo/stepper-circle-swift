041d8752eae9a7523573f97a75e2b724d630766b
Wesley Araujo <wparaujo@gmail.com>
2020-01-21 11:01:45 -0300

    Initial Commit

---
+++ circular-progress-bar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  circular-progress-bar
+//
+//  Created by mac on 21/01/20.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
